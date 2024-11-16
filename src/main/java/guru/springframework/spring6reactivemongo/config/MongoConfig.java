95cc9d01d1e68258f7fcaedaecc252d34d5b58b5
dboeckli <dboeckli@gmail.com>
2024-11-16 21:12:47 +0100

    experimental

--- src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
+++ src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
@@ -1,10 +1,10 @@
 package guru.springframework.spring6reactivemongo.config;
 
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -14,6 +14,9 @@ import static java.util.Collections.singletonList;
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String test;
+
     @Bean
     public MongoClient mongoClient() {
         return MongoClients.create();
@@ -21,14 +24,14 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        //return "sfg";
-        return "test";
+        return "sfg";
     }
 
     //@Override
     // TODO: add credentials to Mongo db. we get authentication failure
     /* we do not need that configuration because there is no authentication */
     protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        System.out.println("#### Mongo DB URI: " + test);
         builder.applyToClusterSettings(settings -> {
             settings.hosts((singletonList(
                 new ServerAddress("127.0.0.1", 32769)
