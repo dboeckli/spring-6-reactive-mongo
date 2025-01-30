a2d7fb5472844ad153cc552e2d11440b8b3c50e3
Dominique Boeckli <dboeckli@gmail.com>
2025-01-30 16:48:23 +0100

    Feature/better config for allowed origins (#27)
    * improvement config allowed origins

--- src/main/java/guru/springframework/spring6reactivemongo/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6reactivemongo/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package guru.springframework.spring6reactivemongo.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -9,6 +13,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -17,13 +22,19 @@ import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
     private static final List<String> ALLOWED_HEADERS = List.of("*");
     private static final List<String> ALLOWED_METHODS = List.of("POST", "GET", "PUT", "OPTIONS", "DELETE", "PATCH");
 
-    @Value("${security.cors.allowed-origins}")
-    private List<String> allowedOrigins;
+    private final AllowedOriginConfig allowedOriginConfig;
+
+    @PostConstruct
+    public void init() {
+        log.info("### Allowed origins: {}", allowedOriginConfig);
+    }
 
     @Bean
     @Order(1)
@@ -58,7 +69,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedOrigins(allowedOriginConfig.getAllowedOrigins());
         configuration.setAllowedMethods(ALLOWED_METHODS);
         configuration.setAllowedHeaders(ALLOWED_HEADERS);
         configuration.setAllowCredentials(true);
@@ -68,5 +79,11 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-    
+
+    @Component
+    @ConfigurationProperties(prefix = "security.cors")
+    @Data
+    public static class AllowedOriginConfig {
+        private List<String> allowedOrigins;
+    }
 }
