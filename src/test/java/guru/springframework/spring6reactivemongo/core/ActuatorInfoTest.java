31afb2541ad8cb4c040c2ef9439516c762ea89b5
adm-boeckli <dboeckli@gmail.com>
2025-05-22 11:45:39 +0200

    improve tests

--- src/test/java/guru/springframework/spring6reactivemongo/core/ActuatorInfoTest.java
+++ src/test/java/guru/springframework/spring6reactivemongo/core/ActuatorInfoTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -23,25 +24,33 @@ class ActuatorInfoTest {
     private WebTestClient webTestClient;
 
     @Autowired
+    BuildProperties buildProperties;
+
+    @Autowired
     ObjectMapper objectMapper;
 
     @Test
-    void actuatorInfoTest() throws JsonProcessingException {
-        EntityExchangeResult<byte[]> result = webTestClient.get().uri("/actuator/info")
+    void actuatorInfoTest() {
+        webTestClient.get().uri("/actuator/info")
             .exchange()
             .expectStatus().isOk()
             .expectBody()
+            .consumeWith(response -> {
+                String jsonResponse = new String(response.getResponseBody());
+                try {
+                    Object json = objectMapper.readValue(jsonResponse, Object.class);
+                    log.info("Response:\n{}", objectMapper.writeValueAsString(json));
+                } catch (JsonProcessingException e) {
+                    log.error("Error parsing JSON response", e);
+                }
+            })
             .jsonPath("$.git.commit.id").isNotEmpty()
             .jsonPath("$.build.javaVersion").isEqualTo("21")
             .jsonPath("$.build.commit-id").isNotEmpty()
             .jsonPath("$.build.javaVendor").isNotEmpty()
-            .jsonPath("$.build.artifact").isEqualTo("spring-6-reactive-mongo")
-            .jsonPath("$.build.group").isEqualTo("guru.springframework")
-            .returnResult();
+            .jsonPath("$.build.artifact").isEqualTo(buildProperties.getArtifact())
+            .jsonPath("$.build.group").isEqualTo(buildProperties.getGroup());
 
-        String jsonResponse = new String(Objects.requireNonNull(result.getResponseBody()));
-        Object json = objectMapper.readValue(jsonResponse, Object.class);
-        log.info("Response:\n{}", objectMapper.writeValueAsString(json));
     }
 
 
